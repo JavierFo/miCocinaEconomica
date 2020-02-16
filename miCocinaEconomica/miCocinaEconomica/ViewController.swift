de4b15cfd8e1b213273dd90995d46c45ae22469e
JavierFo <pk.gs.xv@gmail.com>
2020-02-15 23:27:06 -0600

    Initial Commit

---
+++ miCocinaEconomica/miCocinaEconomica/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  miCocinaEconomica
+//
+//  Created by Javier Ferrer on 2/15/20.
+//  Copyright © 2020 Javier Ferrer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
