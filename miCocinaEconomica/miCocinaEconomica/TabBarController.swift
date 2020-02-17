c06217a56d028ab84e031a2438bbeff2a77fd49f
JavierFo <pk.gs.xv@gmail.com>
2020-02-17 05:38:03 -0600

    Added Tab Bar Icons

--- miCocinaEconomica/miCocinaEconomica/TabBarController.swift
+++ miCocinaEconomica/miCocinaEconomica/TabBarController.swift
@@ -8,10 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class TabBarController: UITabBarController {
+
+    @IBInspectable var defaultIndex: Int = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        selectedIndex = defaultIndex
         // Do any additional setup after loading the view.
     }
 
