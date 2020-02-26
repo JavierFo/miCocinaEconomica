bc765cd4da5d8940da963a76d02001d981537e57
JavierFo <pk.gs.xv@gmail.com>
2020-02-26 04:33:33 -0600

    Added Couple More Unit tests

--- miCocinaEconomica/miCocinaEconomicaTests/miCocinaEconomicaTests.swift
+++ miCocinaEconomica/miCocinaEconomicaTests/miCocinaEconomicaTests.swift
@@ -57,6 +57,63 @@ class miCocinaEconomicaTests: XCTestCase {
         }
     }
     
+    var sut: URLSession!
+    
+    override func setUp() {
+      super.setUp()
+      sut = URLSession(configuration: .default)
+    }
+
+    override func tearDown() {
+      sut = nil
+      super.tearDown()
+    }
+    
+    func testValidCallToMxMarketsGetsHTTPStatusCode200() {
+      
+      let url =
+        URL(string: "https://mxmarkets.herokuapp.com/api/v1/products/")
+      
+      let promise = expectation(description: "Status code: 200")
+
+    
+      let dataTask = sut.dataTask(with: url!) { data, response, error in
+        
+        if let error = error {
+          XCTFail("Error: \(error.localizedDescription)")
+          return
+        } else if let statusCode = (response as? HTTPURLResponse)?.statusCode {
+          if statusCode == 200 {
+        
+            promise.fulfill()
+          } else {
+            XCTFail("Status code: \(statusCode)")
+          }
+        }
+      }
+      dataTask.resume()
+    
+      wait(for: [promise], timeout: 5)
+    }
+    
+    func testCallToToMxMarketsCompletes() {
+          let url =
+            URL(string: "https://mxmarkets.herokuapp.com/api/v1/products/")
+          let promise = expectation(description: "Completion handler invoked")
+          var statusCode: Int?
+          var responseError: Error?
+
+          let dataTask = sut.dataTask(with: url!) { data, response, error in
+            statusCode = (response as? HTTPURLResponse)?.statusCode
+            responseError = error
+            promise.fulfill()
+          }
+          dataTask.resume()
+          wait(for: [promise], timeout: 5)
+
+          XCTAssertNil(responseError)
+          XCTAssertEqual(statusCode, 200)
+    }
     
 }
 
